2369049eb7d92e55a6524f18e14a2e20ec265618
GraysonB <replaygaming34@gmail.com>
2025-10-02 15:47:42 -0400

    Switching to Mediapipe

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 import cv2
 import customtkinter as ctk
 import openai
-from ultralytics import YOLO
+import mediapipe as mp
 
-model = YOLO("Misc\\yolov8n.pt")
+############################# HAND TRACKING SET UP FROM TUTORIAL ####################################
 
 # Open the default camera
 cam = cv2.VideoCapture(0)
@@ -16,26 +16,27 @@ cam = cv2.VideoCapture(0)
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('Misc\\output.mp4', fourcc, 20.0, (frame_width, frame_height))
+#set up hand tracking and drawing
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+model = mp_hands.Hands()
 
 while True:
-    ret, frame = cam.read()
+    #read a frame from the camera
+    success, img = cam.read()
     
-    results = model(frame)
-    annotated = results[0].plot()
+    if success:
+        RGB_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = model.process(RGB_frame)
+        
+        if result.multi_hand_landmarks:
+            for hand_landmark in result.multi_hand_landmarks:
+                print(hand_landmark)
+                mp_drawing.draw_landmarks(img, hand_landmark, mp_hands.HAND_CONNECTIONS)
 
-    cv2.imshow("Hand Tracking", annotated)
-
-    # Write the frame to the output file
-    out.write(frame)
-
-    # Press 'q' to exit the loop
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.imshow("captured image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 # Release the capture and writer objects
-cam.release()
-out.release()
 cv2.destroyAllWindows()
